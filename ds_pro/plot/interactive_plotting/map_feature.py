d1c5d3dd16dc8e4ccb652bcc6669e5d7ceef2074
ibowen <berwin.lew@gmail.com>
2016-09-07 14:52:18 -0700

    feat: add time series

--- ds_pro/plot/interactive_plotting/map_feature.py
+++
@@ -1,8 +0,0 @@
-import csv
-dict_list = []
-with open('feature_importance.csv', 'rb') as f:
-	reader = csv.reader(f)
-	for row in reader:
-		if not row[0].isdigit():
-			dict_list.append({'key':row[0], 'value':int(row[1])})
-print dict_list
